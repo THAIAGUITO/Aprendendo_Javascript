36c82c46606f08134718e50e5ececd5a68073eb4
THAIAGUITO <tiagosilvasantos159@gmail.com>
2026-08-04 23:52:29 -0300

    Adição do código aql da tabela users do nosso banco de dados de estudo. Futuras tabelas serão adicionadas

---
+++ Aula sobre API e FETCH/API em Banco de dados MYSQL/banco_de_dados/bd.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    telefone VARCHAR(20) NOT NULL
+);
